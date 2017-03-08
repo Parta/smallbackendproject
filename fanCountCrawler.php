61ec241284317029fe04a9983579050d559f1fe4
Eric Lam <ericlam51@gmail.com>
2017-03-08 00:17:03 -0500

    Update crawler to use Company model

--- fanCountCrawler.php
+++ fanCountCrawler.php
@@ -1,30 +1,59 @@
 <?php
-
     $configs = include ('config.php');
     include ('models/FanCount.php');
+    include ('models/Company.php');
+
+    $pageId = '';
+    foreach($argv as $value)
+    {
+
+        list($key, $val) = explode('=', $value);
+
+        $key = str_replace('--','',$key);
+
+
+        if($key == 'page_id')
+            $pageId = $val;
+    }
+
 
     //TODO extract page_id from command
-    $url='https://graph.facebook.com/'.'cocacola'.'?access_token='.$configs->fb_api_id.'|'.$configs->fb_api_secret.'&fields=fan_count';
+    $url='https://graph.facebook.com/'.$pageId.'?access_token='.$configs->fb_api_id.'|'.$configs->fb_api_secret.'&fields=fan_count';
 
     $json = file_get_contents($url);
-    $data = json_decode($json,true);
-    $id;
-    $fanCount;
 
-    foreach($data as $key=>$value){
-        if($key=='id')
-            $id=$value;
-        else if ($key=='fan_count'){
-            $fanCount=$value;
+    if($json){
+        $data = json_decode($json,true);
+        $id;
+        $fanCount;
+
+        foreach($data as $key=>$value){
+            if($key=='id')
+                $id=$value;
+            else if ($key=='fan_count'){
+                $fanCount=$value;
+            }
+
         }
-    }
 
-    if($fanCount){
-        $FanCount = new FanCount();
-        $FanCount->setFanCount($fanCount);
-        $FanCount->setPageId($id);
-        $FanCount->save();
-    }
+        if($id){
+            $Company = new Company();
 
+            if($Company->find($id)->num_rows == 0){
+                $Company->setCompanyName($pageId);
+                $Company->setPageId($id);
+                $Company->save();
+            }
+        }
+
+        if($fanCount){
+            $FanCount = new FanCount();
+            $FanCount->setFanCount($fanCount);
+            $FanCount->setPageId($id);
+            $FanCount->save();
+        }
+    } else {
+        exit("Unable to connect to find site". $pageId);
+    }
 
 ?>
