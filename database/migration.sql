10c3e29f6439ac14f1dd65905e67cec4105aeed6
Eric Lam <ericlam51@gmail.com>
2017-03-08 00:28:25 -0500

    Add cron and db script

---
+++ database/migration.sql
@@ -0,0 +1,16 @@
+create table if not exists company (
+	id INT(11) UNSIGNED NOT NULL,
+	name varchar(110) NOT NULL,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	primary key (id)
+);
+
+create table if not exists fan_count (
+    id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+    fb_page_id INT(11) UNSIGNED NOT NULL,
+    fan_count BIGINT NOT NULL,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    primary key (id),
+    foreign key (fb_page_id) REFERENCES company (id)
+);
+
