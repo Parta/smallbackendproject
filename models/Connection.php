8c94d5314d8d683674f60ee9ff466b77c981423c
Eric Lam <ericlam51@gmail.com>
2017-03-08 00:16:37 -0500

    Update connection class

--- models/Connection.php
+++ models/Connection.php
@@ -1,10 +1,15 @@
 <?php
 
-$config = include ('../config.php');
+$config = include ('config.php');
 Class Connection{
+    private $hostname = 'localhost';
+    private $username = 'root';
+    private $password = '';
+    private $db = 'fan_count';
     protected $conn;
+
     public function connect(){
-        $this->conn = new mysqli($config->host, $config->username, $config->password, $this->dbName);
+        $this->conn = new mysqli($this->hostname, $this->username, $this->password, $this->db);
 
         if ($this->conn->connect_error) {
             die("Connection failed: " . $this->conn->connect_error);
