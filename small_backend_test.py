d87ac7f0f32a734b6f5ae9b858c10352b4604fc7
Newman Thurairatnam <newman.joyal@gmail.com>
2017-05-07 15:08:40 -0400

    Removing credentials

--- small_backend_test.py
+++ small_backend_test.py
@@ -1,27 +1,30 @@
 import http.client
 import json
+import pymongo
 
 conn = http.client.HTTPSConnection("graph.facebook.com")
+headers = {'cache-control': "no-cache"}
 
-headers = {
-    'cache-control': "no-cache",
-    'postman-token': "ad384b13-4696-3574-724f-7f64aac7d2fe"
-}
+with open('credentials.json') as credentials_file:
+    credentials = json.load(credentials_file)
 
-app_id = "440101389678089"
-app_secret = "a8f0e041c0a40a6dd6ff9d3641ce4c48"
+    conn.request("GET",
+                 '/oauth/access_token?client_id={app_id}&client_secret={app_secret}&grant_type=client_credentials'.format(
+                     app_id=credentials.get("app_id"), app_secret=credentials.get("app_secret")), headers=headers)
 
-conn.request("GET",
-             '/oauth/access_token?client_id={app_id}&client_secret={app_secret}&grant_type=client_credentials'.format(
-                 app_id=app_id, app_secret=app_secret),
-             headers=headers)
-res = conn.getresponse()
-data = res.read()
+    res = conn.getresponse()
+    data = res.read()
 
-access_token = json.loads(data.decode("utf-8")).get("access_token")
+    response = json.loads(data.decode("utf-8"))
+    access_token = response.get("access_token")
 
-conn.request("GET", "/v2.9/nytimes?access_token={access_token}".format(access_token=access_token), headers=headers)
+    print(access_token)
 
-res = conn.getresponse()
-data = res.read()
-print(data.decode("utf-8"))
+    conn.request("GET",
+                 "/v2.9/{fan_id}?access_token={access_token}&fields=fan_count&format=table".format(fan_id="cocacola",
+                                                                                                   access_token=access_token),
+                 headers=headers)
+
+    res = conn.getresponse()
+    data = res.read()
+    print(data.decode("utf-8"))
