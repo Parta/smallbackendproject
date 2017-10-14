ebdfa66fc69bb1233a80012d005b44a3efaa7482
Unknown <guillaumeboutin2101@gmail.com>
2017-10-13 23:19:59 -0400

    Printing raw html content of cocacola fan page.

--- src/AppBundle/Command/TestCommand.php
+++ src/AppBundle/Command/TestCommand.php
@@ -6,10 +6,16 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
+use GuzzleHttp\Client;
+
 class TestCommand extends Command
 {
+    protected $client;
+
     protected function configure()
     {
+        $this->initializeHttpClient();
+
         $this
             ->setName("test")
             ->setDescription("Just a test command");
@@ -17,7 +23,19 @@ class TestCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output
-            ->writeln("Test command working!");
+        $response = $this->client->request('get', '/cocacola');
+
+        print_r($response->getBody()->getContents());
+
+        // print_r("Test command still working!");
+        // $output
+        //     ->writeln("Test command working!");
+    }
+
+    protected function initializeHttpClient()
+    {
+        $this->client = new Client([
+            'base_uri' => 'https://www.facebook.com'
+        ]);
     }
 }
