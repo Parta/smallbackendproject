7cb5c7fec3c0f3d0d67ec76eb655e2ad00c075c7
Andrzej Tracz <andrzej.tracz@polcode.net>
2018-12-11 09:56:15 +0100

    htaccess and metrics endpoint change

--- api/src/Controller/ChartController.php
+++ api/src/Controller/ChartController.php
@@ -7,14 +7,19 @@ use App\Api\Request\Data\Chart\GetChartDataApiRequestData;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\View\View;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class ChartController extends FOSRestController {
 
     /**
-     * @Rest\Get("metric-values/{interval}/{ids}", name="api_get_metric_values", defaults={"interval": "week", "ids": null}, requirements={"interval": "week|month"})
+     * @Rest\Get("metric-values", name="api_get_metric_values")
      */
-    public function getMetricValues($interval, $ids, GetChartDataApiHandler $chartDataApiHandler) {
+    public function getMetricValues(Request $request, GetChartDataApiHandler $chartDataApiHandler)
+    {
+        $interval = $request->query->get('interval', 'week');
+        $ids = $request->query->get('ids', []);
+
         $response = $chartDataApiHandler->process(new GetChartDataApiRequestData($interval, $ids));
 
         return View::create($response, Response::HTTP_OK);
